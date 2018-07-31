b2117a645fd66af8e507995f624454bd42618e14
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-31 18:12:35 +0000

    setup sessionscontroller and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+
+  private
+
+  def logged_in?
+  !!session[:user_id]
+  end
+
+  def current_user
+    User.find(sessionp[:user_id]) 
+  end
 end
