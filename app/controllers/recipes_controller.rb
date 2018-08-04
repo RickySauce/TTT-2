0abbb8c3e7f2716f01ebac01a42844494ef4f43d
learn <eric.desimone@cortland.edu>
2018-08-04 06:50:55 -0700

    misc

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+
+  def new
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.new
+  end
+
+  def create
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_171603) do
+ActiveRecord::Schema.define(version: 2018_08_04_134358) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_07_31_171603) do
     t.integer "sub_style_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "sub_styles", force: :cascade do |t|
